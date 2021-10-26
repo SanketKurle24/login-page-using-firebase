1b909e6d5d555834bb34da610c753f4400dff748
Sanket Kurle <66297774+SanketKurle24@users.noreply.github.com>
2021-10-26 17:27:51 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDTLq7vwG_CiXdaj7VEIvQTH06Ypi15doQ",
+    authDomain: "login-241d3.firebaseapp.com",
+    projectId: "login-241d3",
+    storageBucket: "login-241d3.appspot.com",
+    messagingSenderId: "864708885133",
+    appId: "1:864708885133:web:714d1bf215ac561bbc40c4",
+    
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
